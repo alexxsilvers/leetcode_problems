aa723efd5d947dd8362f84718566c56f23277620
Ivanov Aleksey <aleksivanov@ozon.travel>
2020-01-11 09:52:13 +0300

    solve generate parenthesis problem

--- 22. Generate Parentheses/main.go
+++ 22. Generate Parentheses/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "log"
+import (
+	"log"
+)
 
 // Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
 func main() {
@@ -14,10 +16,27 @@ func main() {
 		  "()()()"
 		]
 	*/
+
+	log.Println(generateParenthesis(2))
 }
 
 func generateParenthesis(n int) []string {
-	return nil
+	ans := make([]string, 0)
+	backtracking(&ans, []byte{}, 0, 0, n)
+	return ans
 }
 
+func backtracking(ans *[]string, cur []byte, open, closed, max int) {
+	if len(cur) == max*2 {
+		*ans = append(*ans, string(cur))
+		return
+	}
 
+	if open < max {
+		backtracking(ans, append(cur, '('), open+1, closed, max)
+	}
+
+	if closed < open {
+		backtracking(ans, append(cur, ')'), open, closed+1, max)
+	}
+}
