a3b4a1707e6097591fe4f29d7d18cd3ac65d5805
Alexey Ivanov <alexeyivanov@Alexeys-MacBook-Air.local>
2020-10-01 07:30:27 +0300

    solve more problems

--- contest/main.go
+++ contest/main.go
@@ -1,22 +1,68 @@
 package main
 
-import (
-	"log"
-	"sort"
-)
-
 func main() {
-	log.Println(canMakeArithmeticProgression([]int{3, 5, 1}))
-	log.Println(canMakeArithmeticProgression([]int{3, 5, 1, 2}))
 }
 
-func canMakeArithmeticProgression(arr []int) bool {
-	sort.Ints(arr)
-	diff := arr[1] - arr[0]
-	for i := 1; i < len(arr); i++ {
-		if arr[i]-arr[i-1] != diff {
-			return false
+
+
+//
+//func minOperations(nums []int) int {
+//	op := 0
+//
+//	sort.Ints(nums)
+//
+//	for _, n := range nums {
+//		if n % 2 ==
+//	}
+//
+//	return op
+//}
+
+func findSmallestSetOfVertices(n int, edges [][]int) []int {
+	memo := make(map[int]int, 6)
+	for i := 0; i < n; i++ {
+		memo[i] = 0
+	}
+
+	for _, edge := range edges {
+		memo[edge[1]]++
+	}
+
+	res := make([]int, 0)
+	for k, v := range memo {
+		if v == 0 {
+			res = append(res, k)
 		}
 	}
-	return true
+
+	return res
+}
+
+func thousandSeparator(n int) string {
+	if n == 0 {
+		return "0"
+	}
+
+	res := make([]byte, 0)
+	tCounter := 0
+	for n > 0 {
+		res = append(res, byte(n%10)+'0')
+		tCounter++
+
+		if tCounter%3 == 0 {
+			res = append(res, '.')
+		}
+
+		n = n / 10
+	}
+
+	for i := 0; i < len(res)/2; i++ {
+		res[i], res[len(res)-i-1] = res[len(res)-i-1], res[i]
+	}
+
+	if res[0] == '.' {
+		res = res[1:]
+	}
+
+	return string(res)
 }
